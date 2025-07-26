c9892d424606da4ec1983a929e6c2a0d7e56027e
latesh-31 <latesh312006@gmail.com>
2025-07-26 14:42:57 +0530

    Relax stylelint rules for project compatibility

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,18 @@ module.exports = {
     "stylelint-config-standard"
   ],
   rules: {
-    // Add custom rules here if needed
+    'font-family-name-quotes': null,
+    'no-duplicate-selectors': null,
+    'color-function-alias-notation': null,
+    'color-function-notation': null,
+    'alpha-value-notation': null,
+    'keyframes-name-pattern': null,
+    'declaration-block-single-line-max-declarations': null,
+    'property-no-vendor-prefix': null,
+    'color-hex-length': null,
+    'no-descending-specificity': null,
+    'declaration-block-no-duplicate-properties': null,
+    'rule-empty-line-before': null,
+    'media-feature-range-notation': null
   }
 };
